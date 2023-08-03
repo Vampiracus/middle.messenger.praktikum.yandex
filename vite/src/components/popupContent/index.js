4eff9d550f899e1cac280b5ac35b2f7971d09aad
Кудрин Максим <kudrin.ma2012@ya.ru>
2023-08-04 00:45:38 +0300

    Изменена файловая структура

--- vite/src/components/popupContent/index.js
+++
@@ -1,1 +0,0 @@
-export {default} from './popupContent.js'
--- vite/src/pages/mainPage/components/chattingArea/components/chatsPlug/chatsPlug.js
+++
@@ -1,9 +0,0 @@
-import './chatsPlug.css';
-
-export default function chatsPlug() {
-    return `
-    <div class='chatsPlug'>
-        Выберите чат, чтобы отправить сообщение
-    </div>
-    `
-}
